881a103b22f6d9681bf65b0746e656c9e1005c45
Atharva Lele <itsatharva@gmail.com>
2021-10-27 19:51:32 -0700

    main: accept trace file name from input
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, MSD!" << std::endl;
+    // Input Trace File
+    std::string ip_trace_name;
+    std::ifstream *ip_trace_fstream;
+
+    // Check if input trace file is provided
+    // First argument is always executable name
+    if (argc == 1) {
+        std::cerr << "No input trace file provided!" << std::endl;
+        return 1;
+    }
+
+    // Read the input file name
+    ip_trace_name = argv[1];
+    std::cout << "Input file: " << ip_trace_name << std::endl;
+
+    // Check if the file opens
+    ip_trace_fstream = new std::ifstream(ip_trace_name);
+    if (!ip_trace_fstream->is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
+
+    // Add loop here for RAM events
+
+    // Free the filestream heap
+    delete ip_trace_fstream;
 
     return 0;
 }
