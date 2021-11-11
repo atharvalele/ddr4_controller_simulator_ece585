736f853727ff302a5915e8bc4905304bdca16859
Atharva Lele <itsatharva@gmail.com>
2021-11-11 15:56:02 -0800

    main: update main control loop
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,10 @@ int main(int argc, char *argv[])
     std::string ip_trace_name;
     std::ifstream *ip_trace_fstream;
     std::string ip_string;
+    bool ip_file_read_done {false};
+    // Start off as request processed so
+    // we can take in a first request
+    bool req_fetched {true};
 
     // Check if input trace file is provided
     // First argument is always executable name
@@ -44,15 +48,33 @@ int main(int argc, char *argv[])
         cpu_clock_tick++;
 
         // Check if we can read in a request
-        if (dram_controller.is_queue_full() == false) {
-            if (std::getline(*ip_trace_fstream, ip_string)) {
-                req = read_file(ip_string);
-                dram_controller.queue_add(req);
+        if ((!ip_file_read_done) && (dram_controller.is_queue_full() == false)) {
+            // Is previous request processed?
+            // Read in a new one if it is
+            if (req_fetched == true) {
+                if (std::getline(*ip_trace_fstream, ip_string)) {
+                    req = read_file(ip_string);
+                    req_fetched = false;
+                } else {
+                    ip_file_read_done = true;
+                }
             }
-            if(dram_controller.is_queue_empty() && !(*ip_trace_fstream))
-                break;
+            // Add to the queue if the current CPU clock
+            // is more than the requested clock tick
+            if (cpu_clock_tick >= req.cpu_req_time) {
+                // If queue is empty, we can "jump" time
+                if (dram_controller.is_queue_empty()) {
+                    std::cout << "Jumping to clock tick: " << req.cpu_req_time << std::endl;
+                    cpu_clock_tick = req.cpu_req_time;
+                }
+                dram_controller.queue_add(req);
+                req_fetched = true;
+            }
         } else {
-            // std::cout << "Queue FULL!" << std::endl;
+            // If queue is empty and file is done, it means that
+            // our simulation is over
+            if (dram_controller.is_queue_empty())
+                break;
         }
 
         // Do RAM operations every n-th tick since DRAM clock is slower than CPU clock
