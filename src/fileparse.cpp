37432fcfdcff1b38233c1609596242c2fb44d7f8
Atharva Lele <itsatharva@gmail.com>
2021-11-11 20:00:43 -0800

    fileparse: validate tokens
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- src/fileparse.cpp
+++ src/fileparse.cpp
@@ -21,9 +21,7 @@ request read_file(std::string ip_string)
     std::string tokens[3];
     uint8_t token_count = 0;
 
-    #ifdef DEBUG
     bool token_valid {false};
-    #endif
 
     // Traverse the line and get 3 tokens
     while (pos < ip_string.length()-1) {
@@ -46,9 +44,7 @@ request read_file(std::string ip_string)
             //std::cout << tokens[token_count] << std::endl;
 
             token_count++;
-            #ifdef DEBUG
             token_valid = true;
-            #endif
         }
         else {
             break;
@@ -62,10 +58,12 @@ request read_file(std::string ip_string)
     }
 
     // Get the data into variables
-    req.cpu_req_time = std::stoull(tokens[0], nullptr);
-    req.q_time = 0;
-    req.req_type = (REQ_OP)std::stoi(tokens[1], nullptr);
-    req.address = std::stoull(tokens[2], nullptr, 16);
+    if (token_valid == true) {
+        req.cpu_req_time = std::stoull(tokens[0], nullptr);
+        req.q_time = 0;
+        req.req_type = (REQ_OP)std::stoi(tokens[1], nullptr);
+        req.address = std::stoull(tokens[2], nullptr, 16);
+    }
 
     // Print out to debug
     #ifdef DEBUG
