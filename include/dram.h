196e4253838b7c24a42d012409b2ffeadca7b1d9
Yashodhan <wagleyashodhan@gmail.com>
2021-11-26 14:42:29 -0800

    dram: header: added function defs for dram commands

--- include/dram.h
+++ include/dram.h
@@ -15,23 +15,37 @@ private:
     static constexpr uint8_t QUEUE_SIZE = 16;
     std::vector<request> req_queue;
 
+    /* State of Execution */
+    typedef enum DRAM_STATE {
+        IDLE = 0,
+        PRE,
+        PRE_WAIT,
+        ACT,
+        ACT_WAIT,
+        READ,
+        WRITE
+    } OP_STATE;
+
+    /* State of the FSM - Start with PRECHARGE */
+    OP_STATE state = PRE;
+    
     /* RAM parameters - in DRAM clock cycles */
     static constexpr uint8_t tRC = 76;          // ACTIVATE to ACTIVATE or REF command period 
     static constexpr uint8_t tRAS = 52;         // ACTIVATE to PRECHARGE command period
     static constexpr uint8_t tRRD_L = 6;        // ACTIVATE to ACTIVATE command period for same bank group
-    static constexpr uint8_t tRRD_S = 4;        // ACTIVATE to ACTIVATE command period for same bank group
+    static constexpr uint8_t tRRD_S = 4;        // ACTIVATE to ACTIVATE command period for different bank group
     static constexpr uint8_t tRP = 24;          // PRECHARGE command period
     // Add tRFC here
-    static constexpr uint8_t tCWD = 20;         // Not sure yet
-    static constexpr uint8_t tCAS = 24;
+    static constexpr uint8_t tCWD = 20;         // CAS Write latency
+    static constexpr uint8_t tCAS = 24;         // CAS Latency
     static constexpr uint8_t tRCD = 24;         // ACTIVATE to internal READ/WRITE delay
-    static constexpr uint8_t tWR = 20;
-    static constexpr uint8_t tRTP = 12;
-    static constexpr uint8_t tCCD_L = 8;
-    static constexpr uint8_t tCCD_S = 4;
-    static constexpr uint8_t tBURST = 4;
-    static constexpr uint8_t tWTR_L = 12;
-    static constexpr uint8_t tWTR_S = 4;
+    static constexpr uint8_t tWR = 20;          // WRITE recovery
+    static constexpr uint8_t tRTP = 12;         // READ to PRECHARGE command period
+    static constexpr uint8_t tCCD_L = 8;        // READ to READ command period for same bank group
+    static constexpr uint8_t tCCD_S = 4;        // READ to READ command period for different bank group
+    static constexpr uint8_t tBURST = 4;        // BURST time
+    static constexpr uint8_t tWTR_L = 12;       // WRITE to READ command period for same bank group
+    static constexpr uint8_t tWTR_S = 4;        // WRITE to READ command period for different bank group
     // Add refresh time here
 
 public:
@@ -46,6 +60,12 @@ public:
 
     /* RAM Operation */
     void do_ram_things();
+    void activate(uint64_t bank_group, uint64_t bank, uint64_t row);
+    void precharge(uint64_t bank_group, uint64_t bank);
+    void dram_read(uint64_t bank_group, uint64_t bank, uint64_t column);
+    void dram_write(uint64_t bank_group, uint64_t bank, uint64_t column);
+    /* Extra Credit */
+    void dram_refresh();
 };
 
 #endif
